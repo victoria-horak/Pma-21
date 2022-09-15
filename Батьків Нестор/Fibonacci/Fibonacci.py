d1ea018f6e6cdfcc3f8b382be9fab3904edf8e96
LewisSaber <39595617+LewisSaber@users.noreply.github.com>
2022-09-15 11:12:00 +0300

    Add files via upload

--- Батьків Нестор/Fibonacci/Fibonacci.py
+++ Батьків Нестор/Fibonacci/Fibonacci.py
@@ -1,17 +1,21 @@
 
-def fibonachi(first=0, second=1, length=1, fibonachiLine=""):
+class NegativeLength(Exception):
+    """Raised when the input length is negative"""
+
+
+def fibonachi(first=0, second=1, length=1, fibonachi_line=""):
     if length < 0:
-        raise ValueError("Length cannot be negative")
-    if (len(fibonachiLine) == 0):
-        fibonachiLine += str(first) + ", "
-        fibonachiLine += str(second) + ", "
-    next = first + second
-    fibonachiLine += str(next) + ", "
-    return fibonachi(second, next, length-1, fibonachiLine) if length > 1 else fibonachiLine[:-2]
+        raise NegativeLength("Length cannot be negative")
+    if (len(fibonachi_line) == 0):
+        fibonachi_line += str(first) + ", "
+        fibonachi_line += str(second) + ", "
+    next_num = first + second
+    fibonachi_line += str(next_num) + ", "
+    return fibonachi(second, next_num, length-1, fibonachi_line) if length > 1 else fibonachi_line[:-2]
 
 
 try:
     print(fibonachi(0, 3, 4), "\n")
     print(fibonachi(0, 1, -2))
-except ValueError as e:
+except NegativeLength as e:
     print(e)

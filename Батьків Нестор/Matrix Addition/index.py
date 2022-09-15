d2a2aac8d919d0c90bff27d830a3b675b5d7740d
LewisSaber <39595617+LewisSaber@users.noreply.github.com>
2022-09-15 11:13:09 +0300

    Add files via upload

--- Батьків Нестор/Matrix Addition/index.py
+++ Батьків Нестор/Matrix Addition/index.py
@@ -1,59 +1,67 @@
 import re
 
-path = './Батьків Нестор/Matrix Addition/'
+PATH = './Батьків Нестор/Matrix Addition/'
 
-def stringToMatrix(string):
- height = string.count("\n")  + 1
- readList = re.sub("\s+"," ",string).split(" ")
- length = len(readList) /height
- matrix = []
- iterator = 0
- row = []
- for element in readList:
-    row.append(element)
-    iterator+= 1
-    if iterator >= length:
-        iterator = 0
-        matrix.append(row)
-        row = [] 
- return matrix
 
-def matrixAddition(matrix1,matrix2):
-    print(len(matrix1) )
-    if(len(matrix1) != len(matrix2) or len(matrix1[0]) != len(matrix2[0])  ):
-     raise ValueError("Matrixes have diffrent dimensions")
-    resultMatrix = []
+class DIffrentDImensions(Exception):
+    """Raised when the matrix dimensions are not equal"""
+
+
+def string_to_matrix(string):
+    height = string.count("\n") + 1
+    read_list = re.sub("\s+", " ", string).split(" ")
+    length = len(read_list) / height
+    matrix = []
+    iterator = 0
+    row = []
+    for element in read_list:
+        row.append(element)
+        iterator += 1
+        if iterator >= length:
+            iterator = 0
+            matrix.append(row)
+            row = []
+    return matrix
+
+
+def matrix_addition(matrixA, matrixB):
+    if (len(matrixA) != len(matrixB) or len(matrixA[0]) != len(matrixB[0])):
+        raise DIffrentDImensions("Matrixes have diffrent dimensions")
+    result_matrix = []
     iteratorI = 0
-    for matrix1row in matrix1:
+    for matrix1row in matrixA:
         row = []
         iteratorJ = 0
         for matrix1elem in matrix1row:
-            row.append(int(matrix1elem) + int(matrix2[iteratorI][iteratorJ]))
-            iteratorJ+=1
-        resultMatrix.append(row)
-        iteratorI+=1
-    return resultMatrix
+            row.append(int(matrix1elem) + int(matrixB[iteratorI][iteratorJ]))
+            iteratorJ += 1
+        result_matrix.append(row)
+        iteratorI += 1
+    return result_matrix
 
-def matrixToString(matrix):
+
+def matrix_to_string(matrix):
     string = ""
     for row in matrix:
         for elem in row:
             string += str(elem) + " "
-        string +="\n"
+        string += "\n"
     return string
 
-def writeToFile(text,filePath):
- file = open(filePath, "w")
- file.write(text)
- file.close()
+
+def write_to_file(text, filePath):
+    file = open(filePath, "w")
+    file.write(text)
+    file.close()
 
 
-matrix1 = stringToMatrix(open(path+'matrix.txt').read())
-matrix2 = stringToMatrix(open(path+'matrix2.txt').read())
+matrix1 = string_to_matrix(open(PATH+'matrix.txt').read())
+matrix2 = string_to_matrix(open(PATH+'matrix2.txt').read())
 try:
- writeToFile("Matrix1:\n"+matrixToString(matrix1)+
-                  "\nMatrix2:\n"+matrixToString(matrix2)+
-                  "\nResult Matrix:\n"+matrixToString(matrixAddition(matrix1,matrix2)),
-                  path+"resultMatrix.txt")
-except ValueError as e:
+    write_to_file("Matrix1:\n"+matrix_to_string(matrix1) +
+                  "\nMatrix2:\n"+matrix_to_string(matrix2) +
+                  "\nResult Matrix:\n" +
+                  matrix_to_string(matrix_addition(matrix1, matrix2)),
+                  PATH+"resultMatrix.txt")
+except DIffrentDImensions as e:
     print(e)

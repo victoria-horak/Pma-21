d70258719e71e361fef950f3ec581e13c7aed19f
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2022-09-17 14:08:40 +0300

    Update main.py

--- Фареник Максим/Fibonachi/main.py
+++ Фареник Максим/Fibonachi/main.py
@@ -1,6 +1,9 @@
+class IncorrectLength(Exception):
+    """Raised when the lengths is less or equal 0"""
+
 def fibonachi(first=0, second=1, length=3, line=""):
-    if length < 0:
-        raise Exception("Length is less than 0")
+    if length <= 0:
+        raise IncorrectLength("Length is less or equal 0")
 
     if (len(line) == 0):
         line += str(first) + ", "
@@ -17,8 +20,8 @@ try:
     print(fibonachi(0, 1, 5))
     print(fibonachi(int(num_array[0]),int(num_array[1]),int(num_array[2])))
 
-except Exception as e:
+except IncorrectLength as e:
     print(e)
     f=open('data.txt', 'a')
-    f.write("\nError - Length is less than 0")
+    f.write("\nError - Length is less or equal 0")
     f.close()

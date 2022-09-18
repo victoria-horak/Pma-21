e0438c0fc86b92eed9e5a6b901184b3a2df82487
LewisSaber <39595617+LewisSaber@users.noreply.github.com>
2022-09-18 11:09:27 +0300

    Add files via upload

--- Батьків Нестор/Fibonacci/Fibonacci.py
+++ Батьків Нестор/Fibonacci/Fibonacci.py
@@ -3,7 +3,10 @@ class NegativeLength(Exception):
     """Raised when the input length is negative"""
 
 
-def fibonachi(first=0, second=1, length=1, fibonachi_line=""):
+PATH = './Батьків Нестор/Fibonacci/'
+
+
+def fibonachi_internal(first, second, length, fibonachi_line=""):
     if length < 0:
         raise NegativeLength("Length cannot be negative")
     if (len(fibonachi_line) == 0):
@@ -11,11 +14,26 @@ def fibonachi(first=0, second=1, length=1, fibonachi_line=""):
         fibonachi_line += str(second) + ", "
     next_num = first + second
     fibonachi_line += str(next_num) + ", "
-    return fibonachi(second, next_num, length-1, fibonachi_line) if length > 1 else fibonachi_line[:-2]
+    return fibonachi_internal(second, next_num, length-1, fibonachi_line) if length > 1 else fibonachi_line[:-2]
 
 
-try:
-    print(fibonachi(0, 3, 4), "\n")
-    print(fibonachi(0, 1, -2))
-except NegativeLength as e:
-    print(e)
+def add_to_file(text):
+    file = open(PATH+"result.txt", "a")
+    file.write(str(text) + "\n")
+    file.close()
+
+
+def fibonachi(first=0, second=1, length=1):
+    try:
+        result = fibonachi_internal(first, second, length)
+        print(result)
+        add_to_file(result)
+    except NegativeLength as error:
+        print(error)
+        add_to_file(error)
+
+
+fibonachi(1, 0, 3)
+fibonachi(-2, 5, -2)
+fibonachi(7, 12, 14)
+fibonachi(7, 16, 35)

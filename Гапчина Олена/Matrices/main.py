312fba5c75d33dff554242c7c2b7699ba5b9c152
OlenaHapchyna <113590473+OlenaHapchyna@users.noreply.github.com>
2022-09-29 16:32:58 +0300

    Update main.py

--- Гапчина Олена/Matrices/main.py
+++ Гапчина Олена/Matrices/main.py
@@ -1,30 +1,40 @@
-def read_from_file(file):
-    with open(file, 'r') as f:
-        matrix = [[int(num) for num in matrix_str.split()] for matrix_str in f]
-    return matrix
+class WrongSize(Exception):
+    """The size of two matrices is not the same"""
 
 
-def output_result(matrix, file):
-    file = open(file, 'a')
-    for i in matrix:
-        file.write(str(i) + '\n')
+def add_two_matrices(mtrx1, mtrx2):
+    if len(mtrx1) != len(mtrx2) or len(mtrx1[0]) != len(mtrx2[0]):
+        raise WrongSize("The matrices have different size")
+    result = [[0] * len(mtrx1[i]) for i in range(len(mtrx1))]
+    for i in range(len(mtrx1)):
+        for j in range(len(mtrx1[i])):
+            result[i][j] = mtrx1[i][j] + mtrx2[i][j]
+    return result
 
 
-def add_two_matrices(matrix_one, matrix_two):
-    resulting_matrix = []
-    for i in range(len(matrix_one)):
-        row = []
-        for j in range(len(matrix_one[0])):
-            row.append(0)
-        resulting_matrix.append(row)
+print("Matrix 1:")
+file = open('matrix1.txt', 'r')
+print(file.read())
+file.close()
+print("\nMatrix 2:")
+file = open('matrix2.txt', 'r')
+print(file.read())
+file.close()
 
-    for i in range(len(matrix_one)):
-        for j in range(len(matrix_one[0])):
-            resulting_matrix[i][j] = matrix_one[i][j] + matrix_two[i][j]
-    return resulting_matrix
+with open('matrix1.txt') as f:
+    matrix1 = [list(map(int, row.split())) for row in f.readlines()]
+with open('matrix2.txt') as f:
+    matrix2 = [list(map(int, row.split())) for row in f.readlines()]
 
+print("\nResult:")
+file = open("resulting_matrix.txt", 'w')
+try:
+    resulting_matrix = add_two_matrices(matrix1, matrix2)
+    for row in range(len(resulting_matrix)):
+        file.write(str(resulting_matrix[row]) + '\n')
+        print(resulting_matrix[row])
+except WrongSize as error:
+    print(error)
+    file.write(str(error))
 
-matrix1 = read_from_file('matrix1.txt')
-matrix2 = read_from_file('matrix2.txt')
-add_matrices = add_two_matrices(matrix1, matrix2)
-output_result(add_matrices, 'result.txt')
+file.close()

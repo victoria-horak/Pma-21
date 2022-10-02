ff4519772b2c1c121fe460dcecf3e5b2e33a55d7
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2022-10-02 21:23:20 +0300

    Update Matrix.py

--- Піняга Володимир/Class_Matrix_And_Vector/Matrix.py
+++ Піняга Володимир/Class_Matrix_And_Vector/Matrix.py
@@ -1,28 +1,25 @@
-class differSize(Exception): #для вип. різних розмірів у матрицях
-    pass
-
+from differSize import *
 
 class Matrix:
-    def __init__(self, row, column):
-        self.row = row
-        self.column = column
-        self.matrix = []
+    def __init__(self, matrix):
+        self.matrix = matrix
 
 
-    def Enter_Matrix(self, fileName):  # ввід з файлу
+    def read_Matrix(self, fileName):  # ввід з файлу
         with open(fileName, 'r') as file:
-            matrix = [[int(element) for element in matrix_str.split(' ')] for matrix_str in file]
+            matrix = [[element for element in matrix_str.strip().split(',')] for matrix_str in file]
         return matrix
 
 
-    def write(self, matrix, fileName):  # запис у файл результату
+    def write_to_file(self, matrix, fileName):  # запис у файл результату
         file = open(fileName, 'a')
         for iter in matrix:
             file.write(str(iter) + '\n')
+        file.close()
 
 
-    def Add_2matrix(self, mat1, mat2, fileName):  # дод. 2 матриць
-        if (len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0])):
+    def __add__(self, others, fileName):
+        if (len(self.matrix) != len(others.matrix) or len(self.matrix[0]) != len(others.matrix[0])):
             file = open(fileName, 'a')
             file.write(str(differSize("Matrixes don`t have same size!")) + '\n')
 
@@ -30,31 +27,32 @@ class Matrix:
 
         else:
             res_matrix = []
-            for iter in range(len(mat1)):
+            for iter in range(len(matrix)):
                 row = []
-                for jter in range(len(mat1[0])):
+                for jter in range(len(self.matrix[0])):
                     row.append(0)
                 res_matrix.append(row)
-        for iter in range(len(mat1)):
-            for jter in range(len(mat1[0])):
-                res_matrix[iter][jter] = mat1[iter][jter] + mat2[iter][jter]
+        for iter in range(len(self.matrix)):
+            for jter in range(len(self.matrix)):
+                res_matrix[iter][jter] = self.matrix[iter][jter] + others.matrix[iter][jter]
         return res_matrix
 
 
-    def Substract_2matrix(self, mat1, mat2, fileName):  # відн. 2 матриць
-        if (len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0])):
+    def __sub__(self, others, fileName):
+        if (len(self.matrix) != len(others.matrix) or len(self.matrix[0]) != len(others.matrix[0])):
             file = open(fileName, 'a')
             file.write(str(differSize("Matrixes don`t have same size!")) + '\n')
+
             raise differSize("Matrixes don`t have same size!")
 
         else:
             res_matrix = []
-            for iter in range(len(mat1)):
+            for iter in range(len(matrix)):
                 row = []
-                for jter in range(len(mat1[0])):
+                for jter in range(len(self.matrix[0])):
                     row.append(0)
                 res_matrix.append(row)
-        for iter in range(len(mat1)):
-            for jter in range(len(mat1[0])):
-                res_matrix[iter][jter] = mat1[iter][jter] - mat2[iter][jter]
+        for iter in range(len(self.matrix)):
+            for jter in range(len(self.matrix)):
+                res_matrix[iter][jter] = self.matrix[iter][jter] - others.matrix[iter][jter]
         return res_matrix

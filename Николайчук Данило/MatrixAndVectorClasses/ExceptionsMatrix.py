e4738bb3ef61533baa2e9b80779a84ba44726d5f
Danylo <98780995+Den-Nyk@users.noreply.github.com>
2022-10-03 01:36:24 +0300

    Add files via upload

---
+++ Николайчук Данило/MatrixAndVectorClasses/ExceptionsMatrix.py
@@ -0,0 +1,11 @@
+class IfMatrixAreNotSameSize(Exception):
+    "Raise when size of matrix isn`t same."
+    pass
+
+class IfLengthIsZero(Exception):
+    "Raise when matrix is empty."
+    pass
+
+class NotMatrix(Exception):
+    "Raise when passing number as an argument"
+    pass
---
+++ Николайчук Данило/MatrixAndVectorClasses/ExceptionsVector.py
@@ -0,0 +1,11 @@
+class NotVecto(Exception):
+    'raise when not vector.'
+    pass
+
+class IfVectorsAreNotSameSize(Exception):
+    "Raise when size of matrix isn`t same."
+    pass
+
+class IfLengthIsZero(Exception):
+    "Raise when matrix is empty."
+    pass

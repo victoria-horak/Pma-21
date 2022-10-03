f1d9af24908e3fe53c28fb6c31d61285cab03c2e
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2022-10-03 10:20:10 +0300

    Update Matrix.py

--- Піняга Володимир/Class_Matrix_And_Vector/Matrix.py
+++ Піняга Володимир/Class_Matrix_And_Vector/Matrix.py
@@ -1,41 +1,53 @@
 from differSize import *
 
-class Matrix:
+class Matrix():
+
     def __init__(self, matrix):
         self.matrix = matrix
 
 
-    def read_Matrix(self, fileName):  # ввід з файлу
+    def read_matrix(self, fileName):
         with open(fileName, 'r') as file:
-            matrix = [[element for element in matrix_str.strip().split(',')] for matrix_str in file]
-        return matrix
+            for line in file:
+                temporary = [elem for elem in line.strip().split(",")]
+                temporary = [int(element) for element in temporary if element]
+                self.matrix.append(temporary)
+            self.matrix = [elem for elem in self.matrix if elem]
+        file.close()
+        return self.matrix
 
 
-    def write_to_file(self, matrix, fileName):  # запис у файл результату
+    def __str__(self):
+        str_matrix = ''
+        for iterator in self.matrix:
+            str_matrix += str(iterator) + '\n'
+        return str_matrix
+
+
+    def write(self, fileName):
         file = open(fileName, 'a')
-        for iter in matrix:
-            file.write(str(iter) + '\n')
+        for iterator in self.matrix:
+            file.write(str(iterator) + '\n')
+        file.write('\n')
         file.close()
 
 
-    def __add__(self, others, fileName):
-        if (len(self.matrix) != len(others.matrix) or len(self.matrix[0]) != len(others.matrix[0])):
-            file = open(fileName, 'a')
-            file.write(str(differSize("Matrixes don`t have same size!")) + '\n')
-
-            raise differSize("Matrixes don`t have same size!")
-
+    def __add__(self, others):
+        if len(self.matrix) == len(others.matrix) and len(self.matrix[0]) == len(others.matrix[0]):
+            first = [[self.matrix[iterator][jterator] + others.matrix[iterator][jterator] for jterator in
+                        range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
+            return Matrix(first)
         else:
-            res_matrix = []
-            for iter in range(len(matrix)):
-                row = []
-                for jter in range(len(self.matrix[0])):
-                    row.append(0)
-                res_matrix.append(row)
-        for iter in range(len(self.matrix)):
-            for jter in range(len(self.matrix)):
-                res_matrix[iter][jter] = self.matrix[iter][jter] + others.matrix[iter][jter]
-        return res_matrix
+            raise differSize("\nMatrixes don`t have the same size!")
+
+
+    def __sub__(self, other):
+        if len(self.matrix) == len(other.matrix) and len(self.matrix[0]) == len(other.matrix[0]):
+            first = [[self.matrix[iterator][jterator] - other.matrix[iterator][jterator] for jterator in
+                      range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
+            return Matrix(first)
+        else:
+            raise differSize("\nMatrixes don`t have the same size!")
 
 
     def __sub__(self, others, fileName):

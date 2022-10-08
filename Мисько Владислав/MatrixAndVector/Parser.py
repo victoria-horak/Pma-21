bf4da4a3d7276183e7702ab6f783fbdae72efb43
vladmysko123 <113307395+vladmysko123@users.noreply.github.com>
2022-10-08 15:18:04 +0300

    Update Parser.py

--- Мисько Владислав/MatrixAndVector/Parser.py
+++ Мисько Владислав/MatrixAndVector/Parser.py
@@ -1,17 +1,7 @@
 def ParseRow(thelist):
         array = []
-        currentNumber = 0
-        flag = False
-        for element in thelist:
-            if (element != ' ' and element != '\n'):
-                currentNumber *= 10
-                flag = True
-                currentNumber += int(element)
-            else: 
-                if (flag):
-                   array.append(int(currentNumber))
-                currentNumber = 0
-                flag = False
-        if (thelist[thelist.__len__() - 1] != '\n' and thelist[thelist.__len__() - 1] != ' '):
-            array.append(currentNumber)
+        thelist = thelist.strip()
+        array = thelist.split(' ')
+        array = list(filter((' ').__ne__, array))
+        array = list(filter(('').__ne__, array))
         return array

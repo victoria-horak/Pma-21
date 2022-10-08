a1f76b5caf5fc4299fefd8e2d22a6bd1423bec0e
vladmysko123 <113307395+vladmysko123@users.noreply.github.com>
2022-10-08 15:19:01 +0300

    Update main.py

--- Мисько Владислав/MatrixAndVector/main.py
+++ Мисько Владислав/MatrixAndVector/main.py
@@ -2,32 +2,37 @@ from Matrix import Matrix
 from Vector import Vector
 from DifferentDimensionsException import DifferentDimensionsException
 from DifferentLengthsException import DifferentLengthsException
+from NonValidDimensionsForMultiplicationException import NonValidDimensionsForMultiplicationException
 
-resultForMatrices = open('resultForMatrices.txt', 'w')
-resultForVectors = open('resultForVectors.txt', 'w')
+resultForMatrices = open('resultForMatrices.txt', 'a')
+resultForVectors = open('resultForVectors.txt', 'a')
 
 try:
     firstMatrix = Matrix()
     secondMatrix = Matrix()
 
-    firstMatrix.ReadFromFile("Matrix1.txt")
-    secondMatrix.ReadFromFile("Matrix2.txt")
+    firstMatrix.readFromFile("Matrix1.txt")
+    secondMatrix.readFromFile("Matrix2.txt")
 
-    resultMatrix = firstMatrix + secondMatrix
-    resultMatrix.WriteToFile("resultForMatrices.txt")
-    print('Two matrices are added')
+    resultMatrix = firstMatrix.divideMatrices_NonStatic(secondMatrix)
+    resultMatrix.writeToFile("resultForMatrices.txt")
+    print('Two matrices are multiplied')
 except DifferentDimensionsException as e:
-    resultForVectors.write(str(e))
+    resultForMatrices.write(str(e))
+except NonValidDimensionsForMultiplicationException as e:
+    resultForMatrices.write(str(e))
 
 try:
     firstVector = Vector()
     secondVector = Vector()
 
-    firstVector.ReadFromFile('Vector1.txt') 
-    secondVector.ReadFromFile('Vector2.txt')
-    
-    resultVector = firstVector + secondVector
-    resultVector.WriteToFile('resultForVectors.txt')
-    print('Two vectors are added')
+    firstVector.readFromFile('Vector1.txt') 
+    secondVector.readFromFile('Vector2.txt')
+    resultVector = Vector.divideVectors_Static(firstVector, secondVector)
+    resultOfMultiplication = Vector.multiplyVector_Static(firstVector, secondVector)
+    resultVector.writeToFile('resultForVectors.txt')
+
+    resultForVectors.write('result of multiplication: ' + str(resultOfMultiplication))
+    resultForVectors.close()
 except DifferentLengthsException as e:
-    resultForMatrices.write(str(e))
+    resultForVectors.write(str(e))

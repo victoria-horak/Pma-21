52b60edf19aaf5b5a35974de474853ff26e5037a
vladmysko123 <113307395+vladmysko123@users.noreply.github.com>
2022-10-08 15:18:31 +0300

    Update Vector.py

--- Мисько Владислав/MatrixAndVector/Vector.py
+++ Мисько Владислав/MatrixAndVector/Vector.py
@@ -1,5 +1,6 @@
 from Parser import ParseRow
 from DifferentLengthsException import DifferentLengthsException
+from NonValidDimensionsForMultiplicationException import NonValidDimensionsForMultiplicationException
 
 class Vector:
     def __init__(self, vector=None):
@@ -7,17 +8,20 @@ class Vector:
             self.vector = []
         else:
             self.vector = vector
-    def ReadFromFile(self, fileName):
+    def getLength(self):
+        return self.vector.__len__()
+    def readFromFile(self, fileName):
         with open(fileName) as fileToReadFrom:
             lines = fileToReadFrom.readlines()
             if (lines.__len__() > 1):
                 for line in lines:
                     currentRow = ParseRow(line)
                     if (currentRow == []): continue
-                    self.vector.append(int(currentRow[0]))
+                    for element in currentRow:
+                        self.vector.append(float(element))
             else:
                self.vector = ParseRow(lines[0])
-    def WriteToFile(self, fileName):
+    def writeToFile(self, fileName):
         with open(fileName, "w") as fileToWriteTo:
             fileToWriteTo.write(str(self))
     def __add__(self, other):
@@ -27,17 +31,53 @@ class Vector:
             raise DifferentLengthsException('Lengths of vectors are different')
         
         for iterator in range(0, self.vector.__len__()):
-            resultVector.append(int(self.vector[iterator]) + int(other.vector[iterator]))
+            resultVector.append(float(self.vector[iterator]) + float(other.vector[iterator]))
         return Vector(resultVector)
     def __sub__(self, other):
         resultVector = []
         if (self.vector.__len__() != other.vector.__len__()):
             raise DifferentLengthsException('Lengths of vectors are different')
         for iterator in range(0, self.vector.__len__()):
-            resultVector.append(int(self.vector[iterator]) - int(other.vector[iterator]))
+            resultVector.append(float(self.vector[iterator]) - float(other.vector[iterator]))
         return Vector(resultVector)
     def __str__(self):
         result = ''
         for element in self.vector:
-            result += str(element) + ' '
+            result += str(element) + '\n'
         return result
+    @staticmethod
+    def addVectors_Static(firstVector, secondVector):
+        return firstVector + secondVector
+    @staticmethod
+    def subVectors_Static(firstVector, secondVector):
+        return firstVector + secondVector
+    @classmethod
+    def addVectors_NonStatic(self, other):
+        return self + other
+    @classmethod
+    def subVectors_NonStatic(self, other):
+        return self - other
+    def __mul__(self, other):
+        result = 0
+        if (self.getLength() != other.getLength()):
+            raise DifferentLengthsException('Lengths are different')
+        for iterator in range(self.getLength()):
+            result += float(self.vector[iterator]) * float(other.vector[iterator])
+        return result
+    def __truediv__(self, other):
+        result = []
+        if (self.getLength() != other.getLength()):
+            raise DifferentLengthsException('Lengths are different')
+        for iterator in range(self.getLength()):
+            result.append(float(self.vector[iterator]) / float(other.vector[iterator]))
+        return Vector(result)
+    @staticmethod
+    def multiplyVector_Static(firstVector, secondVector):
+        return firstVector * secondVector
+    def multiplyVector_NonStatic(self, other):
+        return self * other
+    @staticmethod
+    def divideVectors_Static(firstVector, secondVector):
+        return firstVector / secondVector
+    def divideVector_NonStatic(self, other):
+        return self / other

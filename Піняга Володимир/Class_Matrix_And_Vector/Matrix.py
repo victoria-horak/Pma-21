19cbd7c3b26dfe6f19cf97d455bf91b77ba07451
VolodymyrPinyaha23 <113587271+VolodymyrPinyaha23@users.noreply.github.com>
2022-10-09 17:46:28 +0300

    Update Matrix.py

--- Піняга Володимир/Class_Matrix_And_Vector/Matrix.py
+++ Піняга Володимир/Class_Matrix_And_Vector/Matrix.py
@@ -1,10 +1,11 @@
 from differSize import *
+from InvalidDimensionsForMatrixMultiplication import *
 
-class Matrix():
+
+class Matrix:
 
     def __init__(self, matrix):
         self.matrix = matrix
-
 
     def read_matrix(self, fileName):
         with open(fileName, 'r') as file:
@@ -16,13 +17,17 @@ class Matrix():
         file.close()
         return self.matrix
 
-
     def __str__(self):
         str_matrix = ''
         for iterator in self.matrix:
             str_matrix += str(iterator) + '\n'
         return str_matrix
 
+    def __len__(self):
+        return len(self.matrix)
+
+    def get_matrix(self):
+        return self.matrix
 
     def write(self, fileName):
         file = open(fileName, 'a')
@@ -31,15 +36,43 @@ class Matrix():
         file.write('\n')
         file.close()
 
-
-    def __add__(self, others):
-        if len(self.matrix) == len(others.matrix) and len(self.matrix[0]) == len(others.matrix[0]):
+    @classmethod
+    def add_static(cls, first_matrix: "Matrix", second_matrix: "Matrix") -> "Matrix":
+        if len(first_matrix) == len(second_matrix):
             first = [[self.matrix[iterator][jterator] + others.matrix[iterator][jterator] for jterator in
-                        range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
+                      range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
+            return cls(first)
+        else:
+            raise differSize("\nMatrices don`t have the same size!")
+
+    @classmethod
+    def sub_static(cls, first_matrix: "Matrix", second_matrix: "Matrix") -> "Matrix":
+        if len(first_matrix) == len(second_matrix):
+            first = [[self.matrix[iterator][jterator] - others.matrix[iterator][jterator] for jterator in
+                      range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
+            return cls(first)
+        else:
+            raise differSize("\nMatrices don`t have the same size!")
+
+    @classmethod
+    def mul_static(cls, matrix1: "Matrix", matrix2: "Matrix") -> "Matrix":
+        if len(matrix1) == len(matrix2) and len(matrix1[0]) == len(matrix2[0]):
+            result = []
+            for iterator in range(len(matrix1)):
+                for jterator in range(len(matrix1[0])):
+                    for kterator in range(len(matrix1)):
+                        result.append(matrix1[iterator][kterator] * matrix2[kterator][jterator])
+            return cls(result)
+        else:
+            raise differSize("\nMatrices don`t have the same size!")
+
+    def __add__(self, other):
+        if len(self.matrix) == len(other.matrix) and len(self.matrix[0]) == len(other.matrix[0]):
+            first = [[self.matrix[iterator][jterator] + other.matrix[iterator][jterator] for jterator in
+                      range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
             return Matrix(first)
         else:
-            raise differSize("\nMatrixes don`t have the same size!")
-
+            raise differSize("\nMatrices don`t have the same size!")
 
     def __sub__(self, other):
         if len(self.matrix) == len(other.matrix) and len(self.matrix[0]) == len(other.matrix[0]):
@@ -47,24 +80,63 @@ class Matrix():
                       range(len(self.matrix[0]))] for iterator in range(len(self.matrix))]
             return Matrix(first)
         else:
-            raise differSize("\nMatrixes don`t have the same size!")
+            raise differSize("\nMatrices don`t have the same size!")
 
+    # def __mul__(self, other):
+    #     """Множення матриць"""
+    #     if len(self.matrix) == len(other.matrix[0]) and len(self.matrix[0]) == len(other.matrix):
+    #         result_matrix = [[0 for col in range(len(other.matrix[0]))] for row in range(len(self.matrix))]
+    #         for rows_of_first in range(len(self.matrix)):
+    #             for col_of_second in range(len(other.matrix[rows_of_first])):
+    #                 for iterator in range(len(other.matrix)):
+    #                     result_matrix[rows_of_first][col_of_second] += \
+    #                         self.matrix[rows_of_first][iterator] * other.matrix[iterator][col_of_second]
+    #         return Matrix(result_matrix)
+    #     else:
+    #         raise InvalidDimensionsForMatrixMultiplication("\nMatrices don`t have the valid size for multiplication!")
 
-    def __sub__(self, others, fileName):
-        if (len(self.matrix) != len(others.matrix) or len(self.matrix[0]) != len(others.matrix[0])):
-            file = open(fileName, 'a')
-            file.write(str(differSize("Matrixes don`t have same size!")) + '\n')
-
-            raise differSize("Matrixes don`t have same size!")
-
+    def __mul__(self, other):
+        if len(self.matrix) == len(other.matrix[0]) and len(self.matrix[0]) == len(other.matrix):
+            result = []
+            for row_of_matrix in range(len(self.matrix)):
+                result.append([0 for column_of_matrix in range(len(other.matrix[0]))])
+            # print("RESULT BEFORE:\n", result)
+            for iterator in range(len(self.matrix)):
+                for jterator in range(len(other.matrix[iterator])):
+                    for kterator in range(len(other.matrix)):
+                        result[iterator][jterator] += self.matrix[iterator][kterator] * other.matrix[kterator][jterator]
+            return Matrix(result)
         else:
-            res_matrix = []
-            for iter in range(len(matrix)):
-                row = []
-                for jter in range(len(self.matrix[0])):
-                    row.append(0)
-                res_matrix.append(row)
-        for iter in range(len(self.matrix)):
-            for jter in range(len(self.matrix)):
-                res_matrix[iter][jter] = self.matrix[iter][jter] - others.matrix[iter][jter]
-        return res_matrix
+            raise InvalidDimensionsForMatrixMultiplication("\nMatrices don`t have the valid size for multiplication!")
+
+    def __truediv__(self, other):
+        try:
+            if len(self.matrix) == len(other.matrix[0]) and len(self.matrix[0]) == len(other.matrix):
+                result = []
+                for row_of_matrix in range(len(self.matrix)):
+                    result.append([0 for column_of_matrix in range(len(other.matrix[0]))])
+                # print("RESULT BEFORE:\n", result)
+                for iterator in range(len(self.matrix)):
+                    for jterator in range(len(other.matrix[iterator])):
+                        for kterator in range(len(other.matrix)):
+                            result[iterator][jterator] += self.matrix[iterator][kterator] / other.matrix[kterator][
+                                jterator]
+                return Matrix(result)
+            else:
+                raise InvalidDimensionsForMatrixMultiplication("\nMatrices don`t have the valid size for "
+                                                               "multiplication!")
+        except ZeroDivisionError:
+            with open('result_matrix.txt', "a+") as file_matrix:
+                file_matrix.write("We cannot divide by zero!")
+            return Matrix([])
+
+    @property
+    def transpose_matrix(self):
+        matrix1 = self.matrix
+        for iterator in range(len(matrix[0])):
+            matrix2 = self.matrix
+            for jterator in matrix:
+                matrix2.append(jterator[iterator])
+            matrix1.append(matrix2)
+        return matrix1
+

cbb2e71e9cc2e2ad8b3812b58d82d612ab37f210
Mykola Buvanyk <61921289+MykolaBuvanyk@users.noreply.github.com>
2022-10-09 23:45:48 +0300

    Update main.py

--- Буваник Микола/Матриця І Вектор Класи/main.py
+++ Буваник Микола/Матриця І Вектор Класи/main.py
@@ -1,18 +1,19 @@
 from Matrix import Matrix
+from lengthNotMatchExeption import LengthNotMatchExeption
 from Vector import Vector
 
 try:
-    matrixFirst = Matrix.readFromFile("matrixFirst.txt")
+    matrixFirst=Matrix("matrixFirst.txt")
     print("First Matrix:", matrixFirst, sep="\n")
-    matrixSecond = Matrix.readFromFile("matrixSecond.txt")
+    matrixSecond = Matrix("matrixSecond.txt")
     print("Second Matrix:", matrixSecond, sep="\n")
-    matrixResult = matrixFirst + matrixSecond
-    print("Result Sum Matrix:", matrixResult, sep="\n")
+    matrixResult = matrixFirst / matrixSecond
+    print("Result Div Matrix:", matrixResult, sep="\n")
     matrixResult.writeToFile("ResultMatrix.txt")
-    matrixResult = Matrix.sub_matrix(matrixFirst, matrixSecond)
+    matrixResult = matrixFirst * matrixSecond
+    print("Result Mul Matrix:", matrixResult, sep="\n")
     matrixResult.writeToFile("ResultMatrix.txt")
-    print("Result Sub Matrix:", matrixResult, sep="\n")
-except Exception as e:
+except LengthNotMatchExeption as e:
     print(str(e))
     with open("ResultMatrix.txt", "a") as file:
         file.write(str(e) + "\n")
@@ -21,12 +22,10 @@ try:
     print("First Vector:", vectorFirst, sep="\n")
     vectorSecond = Vector.readFromFile("vectorSecond.txt")
     print("Second Vector:", vectorSecond, sep="\n")
-    vectorResult = Vector.sum_vector(vectorFirst, vectorSecond)
-    print("Result Sum Vector:", vectorResult, sep="\n")
-    vectorResult.writeToFile("ResultVector.txt")
-    vectorResult = Vector.sub_vector(vectorFirst, vectorSecond)
-    print("Result Sub Vector:", vectorResult, sep="\n")
-    vectorResult.writeToFile("ResultVector.txt")
+    vectorResult = vectorFirst*vectorSecond
+    print("Result Mul Vector:", vectorResult, sep="\n")
+    vectorResult = vectorFirst / vectorSecond
+    print("Result div Vector:", vectorResult, sep="\n")
 except Exception as e:
     print(str(e))
     with open("ResultVector.txt", "a") as file:

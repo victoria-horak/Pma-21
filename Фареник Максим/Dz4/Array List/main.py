4f3987410153e320871b0e8ffdac707a3d7bbf9a
Максим Фареник <60815694+maxymfarenyk@users.noreply.github.com>
2022-10-20 17:25:59 +0300

    Update main.py

--- Фареник Максим/Dz4/Array List/main.py
+++ Фареник Максим/Dz4/Array List/main.py
@@ -5,6 +5,7 @@ try:
     arr = ArrayList()
 
     # Append
+    print("Append: ")
     arr.append(3)
     arr.append(4)
     arr.append(77)
@@ -13,32 +14,38 @@ try:
     print(arr)
 
     #Insert
-    arr.insert(0, 50)
+    print("Insert: ")
+    arr.insert(1, 50)
     print(arr)
 
     #Removing by index
+    print("Remove by index: ")
     arr.removeByIndex(1)
     print(arr)
 
     #Removing by value
+    print("Remove by value: ")
     arr.remove(50)
     print(arr)
 
     #Pop
+    print("Pop: ")
     arr.pop()
     print(arr)
 
     #Clear
+    print("Clear: ")
     arr.clear()
     print(arr)
 
     #Find item
+    print("Find item: ")
     arr.append(3)
     arr.append(4)
     arr.append(77)
     arr.append(6)
     arr.append(50)
     print(arr)
-    print(arr.find(4))
+    print(arr.find(-5))
 except IndexException as e:
     print(e)

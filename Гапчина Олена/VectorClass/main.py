0bb2e8bcd871d30d268ea2b1bf49ec716bd4dd24
OlenaHapchyna <113590473+OlenaHapchyna@users.noreply.github.com>
2022-10-23 19:45:37 +0300

    Update main.py

--- Гапчина Олена/VectorClass/main.py
+++ Гапчина Олена/VectorClass/main.py
@@ -1,10 +1,25 @@
-from Vector import Vector
+from Vector import Vector, DifferentSize
 
 first_vector = Vector(Vector.read_from_file("first_vector.txt"))
+print("Vector A: ", end=' ')
+first_vector.print()
+
 second_vector = Vector(Vector.read_from_file("second_vector.txt"))
+print("\nVector B: ", end=' ')
+second_vector.print()
 
-vector_sum = Vector(first_vector.add(second_vector))
-vector_sum.write_to_file("vector_sum.txt")
+try:
+    vector_sum = Vector(first_vector.add(second_vector))
+    vector_sum.write_to_file("vector_sum.txt")
 
-vector_subtraction = Vector(first_vector - second_vector)
-vector_subtraction.write_to_file("vector_subtraction.txt")
+    vector_subtraction = Vector(first_vector.subtract(second_vector))
+    vector_subtraction.write_to_file("vector_subtraction.txt")
+
+    vector_sum = Vector(first_vector.multiply(second_vector))
+    vector_sum.write_to_file("vector_multiplication.txt")
+
+    vector_sum = Vector(first_vector.divide(second_vector))
+    vector_sum.write_to_file("vector_division.txt")
+    
+except DifferentSize as error:
+    print(error)

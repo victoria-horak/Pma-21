9be9b05cdf9952dbd44dd3988ef40ee7754d5ee0
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2022-10-23 19:57:23 +0300

    Add files via upload

--- Дякович Лілія/LinkedList/LinkedList.py
+++ Дякович Лілія/LinkedList/LinkedList.py
@@ -1,17 +1,17 @@
-from IndexError import  IndexError
+from IndexError import IndexError
+
+
 class Node:
-    def __init__(self, data=None, next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
+        self.prev = None
 
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_beginning(self, data):
-        node = Node(data, self.head)
-        self.head = node
 
     def print(self):
         if self.head is None:
@@ -25,19 +25,31 @@ class LinkedList:
 
         print(llstr)
 
-    def insert_at_and(self, data):
+    def append(self, data):
         if self.head is None:
-            self.head = Node(data, None)
-            return
-        itr = self.head
-        while itr.next:
-            itr = itr.next
-        itr.next = Node(data, None)
+            new_node = Node(data)
+            new_node.prev = None
+            self.head = new_node
+        else:
+            new_node = Node(data)
+            itr = self.head
+            while itr.next:
+                itr = itr.next
+            itr.next = new_node
+            new_node.prev = itr
+            new_node.next = None
 
-    def insert_values(self, data_list):
-        self.head = None
-        for data in data_list:
-            self.insert_at_and(data)
+    def prepend(self, data):
+        if self.head is None:
+            new_node = Node(data)
+            new_node.prev = None
+            self.head = new_node
+        else:
+            new_node = Node(data)
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            new_node.prev = None
 
     def get_length(self):
         count = 0
@@ -47,32 +59,104 @@ class LinkedList:
             itr = itr.next
         return count
 
+    def clear(self):
+        self.head = None
+
+    def insert_values(self, data_list):
+        # self.head = None
+        for data in data_list:
+            self.append(data)
+
     def remove_at(self, index):
         if index < 0 or index > self.get_length():
             raise IndexError("index is out of length\n")
         if index == 0:
+            self.head.prev = self.head
             self.head = self.head.next
         count = 0
         itr = self.head
         while itr:
             if count == index - 1:
                 itr.next = itr.next.next
+                itr.prev = itr.next
                 break
             itr = itr.next
             count += 1
 
-    def insert_at(self, index, data):
-        if index < 0 or index > self.get_length():
-            raise IndexError("index is out of length\n")
-        if index == 0:
-            self.insert_at_beginning(data)
+
+    def searchNode(self, value):
+        i = 1
+        flag = False
+        # Node current will point to head
+        current = self.head
+
+        # Checks whether the list is empty
+        if self.head is None:
+            print("List is empty")
             return
-        count = 0
-        itr = self.head
-        while itr:
-            if count == index - 1:
-                node = Node(data, itr.next)
-                itr.next = node
+
+        while current != None:
+            # Compare value to be searched with each node in the list
+            if current.data == value:
+                flag = True
                 break
-            itr = itr.next
-            count += 1
+            current = current.next
+            i = i + 1
+
+        if flag:
+            print("Node is present in the list at the position : " + str(i))
+        else:
+            print("Node is not present in the list")
+
+
+
+
+    def push_at(self, newElement, position):
+        newNode = Node(newElement)
+        if (position < 1):
+            print("\nposition should be >= 1.")
+        elif (position == 1):
+            newNode.next = self.head
+            self.head.prev = newNode
+            self.head = newNode
+        else:
+            temp = self.head
+            for i in range(1, position ):
+                if (temp != None):
+                    temp = temp.next
+            if (temp != None):
+                newNode.next = temp.next
+                newNode.prev = temp
+                temp.next = newNode
+                if (newNode.next != None):
+                    newNode.next.prev = newNode
+            else:
+                print("\nThe previous node is null.")
+
+
+    def pop_all(self, key):
+        # 1. if the head is not null and value stored at head
+        #   is equal to key, keep on adjusting the head as
+        #   head next, delete previous head and adjust links
+        #   until new head becomes null or not equal to key
+        while (self.head != None and self.head.data == key):
+            nodeToDelete = self.head
+            self.head = self.head.next
+            nodeToDelete = None
+            if (self.head != None):
+                self.head.prev = None
+
+        # 2. create a temp node to traverse through the
+        #   list and delete nodes with value equal to
+        #   key and adjust links accordingly
+        temp = self.head
+        if (temp != None):
+            while (temp.next != None):
+                if (temp.next.data == key):
+                    nodeToDelete = temp.next
+                    temp.next = temp.next.next
+                    if (temp.next != None):
+                        temp.next.prev = temp
+                    nodeToDelete = None
+                else:
+                    temp = temp.next
--- Дякович Лілія/LinkedList/main.py
+++ Дякович Лілія/LinkedList/main.py
@@ -1,19 +1,38 @@
 from LinkedList import LinkedList
 from IndexError import IndexError
 
-try:
-    linked_list1 = LinkedList()
-    linked_list2 = LinkedList()
-    linked_list1.insert_at_beginning(34)
-    linked_list1.insert_at_beginning(9)
-    linked_list1.insert_at_and(67)
-    print("length ", linked_list1.get_length())
-    linked_list1.print()
+# try:
+#     linked_list1 = LinkedList()
+#     linked_list2 = LinkedList()
+#     linked_list1.insert_at_beginning(34)
+#     linked_list1.insert_at_beginning(9)
+#     linked_list1.insert_at_and(67)
+#     print("length ", linked_list1.get_length())
+#     linked_list1.print()
+#
+#     # linked_list2.insert_values(["Kolia", "Lily", "Yura", "Vetal"])
+#     # linked_list2.remove_at(0)
+#     # linked_list2.insert_at_beginning(34)
+#     # linked_list2.insert_at(0, "Ann")
+#     # linked_list2.print()
+# except Exception as e:
+#     print(str(e))
 
-    # linked_list2.insert_values(["Kolia", "Lily", "Yura", "Vetal"])
-    # linked_list2.remove_at(0)
-    # linked_list2.insert_at_beginning(34)
-    # linked_list2.insert_at(0, "Ann")
-    # linked_list2.print()
-except Exception as e:
-    print(str(e))
+
+ll1 = LinkedList()
+ll1.append(23)
+ll1.append(24)
+ll1.append(27)
+ll1.append(26)
+ll1.append(27)
+ll1.prepend(27)
+ll1.remove_at(3)
+ll1.remove_at(2)
+ll1.insert_values([23, 67, 89, 65, 45])
+ll1.searchNode(23)
+ll1.push_at(2,2)
+# ll1.pop_all(27)
+print(ll1.get_length())
+
+ll1.print()
+

8f75995e1dcbf0a02f8f976c39e7152e8e925a3c
victoria-horak <113350889+victoria-horak@users.noreply.github.com>
2022-10-30 23:08:43 +0200

    Add files via upload

---
+++ Горак Вікторія/Colour.py
@@ -0,0 +1,6 @@
+class Colour:
+    def __init__(self):
+        self.colour = ""
+
+    def print_(self):
+        pass
---
+++ Горак Вікторія/Yellow.py
@@ -0,0 +1,3 @@
+class Yellow:
+    def __init__(self):
+        self.colour = 'Yellow'

7f31ef268c8eb6916a54ffb7f54e87c1c028b35c
Mykola Buvanyk <61921289+MykolaBuvanyk@users.noreply.github.com>
2022-10-30 15:47:09 +0200

    Update Triangle.py

--- Буваник Микола/Figure/Triangle.py
+++ Буваник Микола/Figure/Triangle.py
@@ -1,14 +1,17 @@
 from Figure import *
-
+from SideTriangleExeption import SideTriangleExeption
 
 class Triangle(Figure):
     type = "Triangle"
 
     def __init__(self, colour: Colour, a, b, c):
         Figure.__init__(self, colour)
-        self.a = a
-        self.b = b
-        self.c = c
+        if c > a + b or a > b + c or b > a + c:
+            raise SideTriangleExeption("Sum two side in triangle need beggest than third side")
+        else:
+            self.a = a
+            self.b = b
+            self.c = c
 
     def Perimetr(self):
         return self.a + self.b + self.c

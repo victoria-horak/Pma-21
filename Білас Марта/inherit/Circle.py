4ba37d25ec16f7f7be1c42ef4a4169e60a299c17
MartaBilas <113312120+MartaBilas@users.noreply.github.com>
2022-11-04 01:18:32 +0200

    Update Circle.py

--- Білас Марта/inherit/Circle.py
+++ Білас Марта/inherit/Circle.py
@@ -8,7 +8,7 @@ class WrongParameters(Exception):
 
 class Circle(Figure):
 
-    def __init__(self, color: Color = "blue", radius=0):
+    def __init__(self, color: Color = Blue(), radius=0):
         try:
             if radius < 0:
                 raise WrongParameters
@@ -17,14 +17,14 @@ class Circle(Figure):
             print(" value error")
         except WrongParameters:
             print("wrong parameters")
-        self.color = Color(color)
+        self.color = color
 
     def __str__(self):
         result = "Circle:" + '\n'
         result += "radius : " + str(self.radius) + '\n'
         result += "color: " + self.color.getColor() + '\n'
         result += "perimeter: " + str(self.perimeter()) + '\n'
-        result += "area: " + str(self.area())
+        result += "area: " + str(self.area()) + '\n'
         return result
 
     def perimeter(self):
@@ -36,4 +36,5 @@ class Circle(Figure):
     def draw(self):
         turtle.color(self.color.getColor())
         turtle.circle(self.radius)
+        time.sleep(3)
         turtle.Screen().reset()

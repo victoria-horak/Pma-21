6fe9ca2e736f2be5ac66fe839d816b7c9d56861e
MartaBilas <113312120+MartaBilas@users.noreply.github.com>
2022-11-04 01:20:20 +0200

    Update Square.py

--- Білас Марта/inherit/Square.py
+++ Білас Марта/inherit/Square.py
@@ -3,7 +3,7 @@ from Rectangle import *
 
 class Square(Rectangle):
 
-    def __init__(self, color: Color = "blue", side=0):
+    def __init__(self, color: Color = Blue(), side=0):
         Rectangle.__init__(self, color, side, side)
 
     def __str__(self):
@@ -11,5 +11,5 @@ class Square(Rectangle):
         result += "side : " + str(self.firstSide) + '\n'
         result += "color: " + self.color.getColor() + '\n'
         result += "perimeter: " + str(self.perimeter()) + '\n'
-        result += "area: " + str(self.area())
+        result += "area: " + str(self.area()) + '\n'
         return result

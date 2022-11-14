bc0483774a45629cd8d872ae649673ef984dc813
PosivnychRoman <113590345+PosivnychRoman@users.noreply.github.com>
2022-11-14 11:35:37 +0200

    Add files via upload

---
+++ Посівнич Роман/Map/ChemicalElementMain.py
@@ -0,0 +1,33 @@
+def readData(fileName):
+    dataDictionary = {}
+    with open(fileName) as file:
+        for line in file:
+            cleanedLine = line.strip()
+            if cleanedLine == "":
+                continue
+            words = line.split()
+            if len(words) != 2:
+                print("Incorrect line")
+                continue
+
+            dataDictionary[words[0]] = words[1]
+    return dataDictionary
+
+
+def getByKey(dictionary: dict, key):
+    if key in dictionary:
+        return dictionary[key]
+    return "Cannot find value"
+
+
+def getByValue(dictionary: dict, value):
+    allValues = list(dictionary.values())
+    if value in allValues:
+        return list(dictionary.keys())[allValues.index(value)]
+    return "Cannot find key"
+
+
+data = readData("Chemical elements.txt")
+print(data)
+print(getByKey(data, "Aurum"))
+print(getByValue(data, "O"))

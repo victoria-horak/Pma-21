43116cd52277e849d537c41f6d21fd61db0ca50a
MartaBilas <113312120+MartaBilas@users.noreply.github.com>
2022-11-20 18:09:42 +0200

    Update main.py

--- Білас Марта/fibonachi/main.py
+++ Білас Марта/fibonachi/main.py
@@ -1,21 +1,23 @@
 class ValueLessThanZero(Exception):
-    "This value can't be zero"
-    pass
+    """This value can't be zero"""
+
 
 def fibonachi(amountOfNumbers=1, firstNumber=0, secondNumber=1):
-    if amountOfNumbers != 1:
+    if amountOfNumbers != 0:
         outputFile.write(str(firstNumber + secondNumber) + " ")
-        return fibonachi(amountOfNumbers-1, secondNumber, firstNumber + secondNumber)
-    return firstNumber+secondNumber
-    outputFile.write(str(firstNumber+secondNumber) + " ")
+        return fibonachi(amountOfNumbers - 1, secondNumber, firstNumber + secondNumber)
+    return firstNumber + secondNumber
+    outputFile.write(str(firstNumber + secondNumber) + " ")
 
-outputFile=open("output.txt", "a")
+
+outputFile = open("output.txt", "w")
 try:
     firstNumber = int(input("enter first number:"))
     secondNumber = int(input("enter second number:"))
     amountOfNumbers = int(input("enter amount number:"))
     if amountOfNumbers < 0:
         raise ValueLessThanZero
+    outputFile.write(str(firstNumber) + " " + str(secondNumber) + " ")
     fibonachi(amountOfNumbers, firstNumber, secondNumber)
 except ValueLessThanZero:
     outputFile.write("this value can't be zero")
@@ -24,5 +26,4 @@ except ValueError as errorMessage:
     outputFile.write(str(errorMessage))
     print("\n""Invalid value")
 outputFile.close()
-
 

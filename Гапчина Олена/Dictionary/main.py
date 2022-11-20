d0ec5fb397165b7c359d84c16dd730ec28c489c3
OlenaHapchyna <113590473+OlenaHapchyna@users.noreply.github.com>
2022-11-20 19:57:01 +0200

    Update main.py

--- Гапчина Олена/Dictionary/main.py
+++ Гапчина Олена/Dictionary/main.py
@@ -1,17 +1,21 @@
 # (1) input a dictionary from file
-dictionary = {}
-with open("elements.txt") as file:
-    for line in file:
-        (key, value) = line.split()
-        if key == "None" or value == "None":
-            continue
-        dictionary[key] = value
-dictionary.update({'Calcium': 'Ca'})
+def input_from_file(file_name):
+    with open(file_name) as file:
+        new_dictionary = {}
+        for line in file:
+            (key, value) = line.split()
+            if key == "None" or value == "None":
+                continue
+            new_dictionary[key] = value
+    return new_dictionary
+
 
 # (2) print a dictionary
-print("Dictionary:")
-for name, symbol in dictionary.items():
-    print(f'{name} - {symbol}')
+def print_dict(dictionary_name):
+    print("Dictionary:")
+    for name, symbol in dictionary_name.items():
+        print(f'{name} - {symbol}')
+
 
 # (3) find symbol of the element by its name
 def find_by_name(dictionary_name):
@@ -31,9 +35,12 @@ def find_by_symbol(dictionary_name):
         print("The element was not found.")
     else:
         for element_name, element_symbol in dictionary_name.items():
-            if element_name == searched_symbol:
+            if element_symbol == searched_symbol:
                 print(f'The symbol for {element_symbol} is {element_name}.')
-                
 
+
+dictionary = input_from_file("elements.txt")
+dictionary.update({'Calcium': 'Ca'})
+print_dict(dictionary)
 find_by_name(dictionary)
 find_by_symbol(dictionary)

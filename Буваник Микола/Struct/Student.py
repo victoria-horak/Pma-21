26421d0d445016c04f477c64c07772dd3d095ec8
Mykola Buvanyk <61921289+MykolaBuvanyk@users.noreply.github.com>
2022-11-21 22:16:57 +0200

    Add files via upload

--- Буваник Микола/Struct/Student.py
+++ Буваник Микола/Struct/Student.py
@@ -1,16 +1,16 @@
 from dataclasses import dataclass
-
+import datetime
 
 @dataclass
 class Student:
     firstName: str
     secondName: str
-    dateOfBirthday: [int]
+    dateOfBirthday: datetime.datetime(1,1,1)
     grades: [int]
 
     def __str__(self):
         string = f"{self.firstName} {self.secondName} \t"
-        string += f"{self.dateOfBirthday[0]}/{self.dateOfBirthday[1]}/{self.dateOfBirthday[2]}\t"
+        string += f"{self.dateOfBirthday}\t"
         string += "Grade: " + " ".join(str(i) for i in self.grades)
         return string
 
@@ -18,6 +18,6 @@ class Student:
         arr = string.split(" ")
         self.firstName = arr[0]
         self.secondName = arr[1]
-        self.dateOfBirthday = [int(x) for x in arr[2:5]]
+        self.dateOfBirthday = datetime.date(int(arr[2]),int(arr[3]),int(arr[4]))
         self.grades = [int(x) for x in arr[5:len(arr)]]
         return self

f4564e6f53360551e435491861fd030d0f1868ec
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2022-11-21 21:02:20 +0200

    Add files via upload

--- Дякович Лілія/request/main.py
+++ Дякович Лілія/request/main.py
@@ -1,18 +1,35 @@
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import FastAPI, Header
 
 app = FastAPI()
 
 
 @app.get("/")
-def sayHi():
-    return "Hi"
+def hello():
+    return "Hello"
+
+
+class Item(BaseModel):
+    name: str
+
+
+app = FastAPI()
+
+
+@app.post("/body/")
+def create_item(item: Item = None):
+    if item is None or item.name is None:
+        return "Hello user"
+    else:
+        return "Hello " + item.name
+
+
+@app.post("/header")
+def read_items(name: str | None = Header(default=None, convert_underscores=False)):
+    return "Hello " + name
 
 
 @app.post("/")
-def sayHitoUser(name=None, firstname=None):
-    if name is None or firstname is None:
-        return "key is None"
-    elif name == "" or firstname == "":
-        return "value is empty"
-    else:
-        return "Hi, " + name + " " + firstname
+def read_items(name: str):
+    return "Hello " + name
+# uvicorn main:app --reload

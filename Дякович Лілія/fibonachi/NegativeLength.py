e679085f6bcd50cd0562978a147ed5cd118b75bb
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2022-11-21 20:58:58 +0200

    Add files via upload

---
+++ Дякович Лілія/fibonachi/NegativeLength.py
@@ -0,0 +1,2 @@
+class NegativeLength(Exception):
+    pass
--- Дякович Лілія/fibonachi/main.py
+++ Дякович Лілія/fibonachi/main.py
@@ -1,29 +1,28 @@
-def fibonachi(count=0, firstNum = 0, secondNum = 1):
+from NegativeLength import NegativeLength
+
+
+def fibonachi(count=0, firstNum=0, secondNum=1):
     if count == 0:
         return firstNum
     if count == 1:
         return secondNum
 
     else:
-        return(fibonachi(count-1, firstNum,secondNum)+fibonachi(count-2,firstNum,secondNum))
+        return (fibonachi(count - 1, firstNum, secondNum) + fibonachi(count - 2, firstNum, secondNum))
 
 
 count = int(input("Cont of numbers fibonachi = "))
-first = int(input("First numeric of fibonachi = "))
-second = int(input("Second numeric of fibonachi = "))
-
-
-class NegativeLength(Exception):
-    pass
+first_fibonaci = int(input("First numeric of fibonachi = "))
+second_fibonaci = int(input("Second numeric of fibonachi = "))
 
 try:
     if count < 0:
-        file = open("error.txt", "a")
         raise NegativeLength("count of numbers is negative")
     else:
         for length in range(count + 2):
-            print(fibonachi(length,first, second))
+            print(fibonachi(length, first_fibonaci, second_fibonaci))
 except NegativeLength as e:
+    file = open("error.txt", "a")
     print(e)
     file.write("\n Error----count is negative----")
-
+    file.close()

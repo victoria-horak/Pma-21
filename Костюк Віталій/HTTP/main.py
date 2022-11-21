be696507447684f42a862ecce35a3bd2a2f7c771
Vitaliy <113024338+vitalikkostiuk7@users.noreply.github.com>
2022-11-21 20:16:11 +0200

    Add files via upload

--- Костюк Віталій/HTTP/main.py
+++ Костюк Віталій/HTTP/main.py
@@ -1,17 +1,55 @@
+from flask import Flask, request
 
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-@app.get("/")
-def default_value():
-    return "Hello"
+app = Flask(__name__)
 
 
-@app.post("/")
-def first_name(first_name):
-    if first_name is None:
-        return "Not given username"
-    else:
-        return "Hello " + first_name
+@app.route('/', methods=['GET', 'POST'])
+def default_route():
+    if request.method == 'GET':
+        return "Hello user"
+    elif request.method == "POST":
+        data = request.form
+        if not data.__contains__('first_name'):
+            return 'Hello user'
+        else:
+            return 'Hello  ' + data['first_name']
+
+
+@app.route('/header', methods=['GET', 'POST'])
+def headers():
+    if request.method == 'GET':
+        return 'Hello user'
+    elif request.method == "POST":
+        data = request.headers
+        if not data.__contains__('first_name'):
+            return 'Hello user from header'
+        else:
+            return 'Hello  ' + data['first_name']
+
+
+@app.route('/params', methods=['GET', 'POST'])
+def params():
+    if request.method == 'GET':
+        return 'Hello user'
+    elif request.method == "POST":
+        data = request.args
+        if not data.__contains__('first_name'):
+            return 'Hello user'
+        else:
+            return 'Hello  ' + data['first_name']
+
+
+@app.route('/json', methods=['GET', 'POST'])
+def json():
+    if request.method == 'GET':
+        return 'Hello user'
+    elif request.method == "POST":
+        data = request.json
+        if data is None or not data.__contains__('first_name'):
+            return 'Hello user from json'
+        else:
+            return 'Hello ' + data['first_name']
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

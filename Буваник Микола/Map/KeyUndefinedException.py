2389f834a416380659bec426b14023d577b44529
Mykola Buvanyk <61921289+MykolaBuvanyk@users.noreply.github.com>
2022-11-21 22:11:25 +0200

    Add files via upload

---
+++ Буваник Микола/Map/KeyUndefinedException.py
@@ -0,0 +1,2 @@
+class KeyUndefinedException(Exception):
+    pass
--- Буваник Микола/Map/main.py
+++ Буваник Микола/Map/main.py
@@ -1,31 +1,42 @@
-from Student import Student
+from KeyUndefinedException import KeyUndefinedException
 
 
-def searcherStudent(arr):
-    newArr = []
-    for element in arr:
-        k = 0
-        for iterator in element.grades:
-            if iterator == 2:
-                k += 1
-        if k > 0:
-            newArr.append(element)
-    return newArr
+def KeySearcher(arr: dict, value: str):
+    for iterator in arr:
+        if str(arr.get(iterator)) == value:
+            return iterator
+    raise KeyUndefinedException("Key not found")
 
 
-arr = []
-with open("data.txt") as file:
-    for line in file:
-        student = Student("", "", [], [])
-        student.readFromString(line)
-        arr.append(student)
-for i in arr:
-    print(i)
-new_arr = searcherStudent(arr)
-print("\n Student who have 2 grade")
-for i in new_arr:
-    print(i)
-new_arr.sort(key=lambda student: student.firstName)
-print("\n Student who have 2 grade sorted by Firstname")
-for i in new_arr:
-    print(i)
+def ValueSearcher(arr: dict, key: str):
+    if key == "None":
+        return "Key is None"
+    for iterator in arr:
+        if str(iterator) == key:
+            return arr.get(iterator)
+    raise KeyUndefinedException("Value not found")
+
+
+def keyIsNone(arr):
+    for iterator in arr:
+        if iterator is None:
+            return arr.pop(None, None)
+
+
+chemistryElement = {}
+with open("data.txt") as f:
+    for line in f:
+        (key, val) = line.split()
+        chemistryElement[key] = val
+    chemistryElement[None] = "value"
+    chemistryElement["Key"] = None
+    keyIsNone(chemistryElement)
+print(chemistryElement)
+try:
+    k = input()
+    if k == "None":
+        raise KeyUndefinedException("Key is None")
+    print(ValueSearcher(chemistryElement, k))
+    print(KeySearcher(chemistryElement, k))
+except Exception as e:
+    print(e)

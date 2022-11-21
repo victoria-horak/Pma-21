b893bb8545fd51f04af6235505866a647ac28720
LiliaDiak <96654700+LiliaDiak@users.noreply.github.com>
2022-11-21 21:00:07 +0200

    Add files via upload

---
+++ Дякович Лілія/map/NoneErrorExpresion.py
@@ -0,0 +1,2 @@
+class NoneErrorExpresion(Exception):
+    pass
--- Дякович Лілія/map/main.py
+++ Дякович Лілія/map/main.py
@@ -1,3 +1,6 @@
+from NoneErrorExpresion import NoneErrorExpresion
+
+
 def read_from_file(file_name):
     with open(file_name, encoding="utf-8") as file:
         dictionary = dict()
@@ -7,23 +10,38 @@ def read_from_file(file_name):
     return dictionary
 
 
+def keyIsNone(mass):
+    for i in mass:
+        if i is None:
+            return mass.pop(None, None)
+
+
 def find_key(dictionary, word):
     for i in dictionary:
         if dictionary.get(i) == word:
             return i
-    return "no key"
+    raise NoneErrorExpresion("key is None")
 
 
 def find_value(dictionary, word):
     for i in dictionary:
         if i == word:
             return dictionary.get(i)
-    return "no value"
+    raise NoneErrorExpresion("value is None")
 
 
-dictionary = read_from_file("data.txt")
-print(dictionary)
-value = input("input value = ")
-print(find_key(dictionary, value))
-key = input("input key = ")
-print(find_value(dictionary, key))
+try:
+    dictionary = read_from_file("data.txt")
+
+    dictionary[None] = "something"
+    dictionary["something"] = None
+    keyIsNone(dictionary)
+    print(dictionary)
+    value = input("input value = ")
+    print(find_key(dictionary, value))
+    key = input("input key = ")
+    print(find_value(dictionary, key))
+    if key=="None":
+        raise NoneErrorExpresion("Key is None")
+except (NoneErrorExpresion) as e:
+    print(e)
